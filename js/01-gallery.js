87d72dfd80f5e163fe98ff472d7ac6e043ec9d29
Spec27 <shkvara27@gmail.com>
2022-01-21 13:03:05 +0200

    20.01.22

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,47 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const createGallery = document.querySelector(".gallery");
+const galleryMarcup = createGalleryCards(galleryItems);
+createGallery.insertAdjacentHTML("beforeend", galleryMarcup);
+createGallery.addEventListener("click", openbigImgEl);
+function createGalleryCards(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `
+        <div class="gallery__item">
+          <a class="gallery__link" href="large-image.jpg">
+            <img
+              class="gallery__image"
+              src="${preview}"
+              data-source="${original}"
+              alt="${description}"
+            />
+          </a>
+        </div>`;
+    })
+    .join("");
+}
+
+function openbigImgEl(event) {
+  event.preventDefault();
+  if (!event.target.classList.contains("gallery__image")) {
+    return;
+  }
+  openBigImgModal(event);
+}
+
+function openBigImgModal(event) {
+  const modal = basicLightbox.create(`<img width="1280" height="968" src= "${event.target.dataset.source}"></img>`);
+  modal.show();
+}
+
+/* 
+1:Создание и рендеринг разметки по массиву данных 
+ galleryItemsи предоставленному шаблону элемента декора.
+2:Реализация делегирования div.galleryи получение urlбольшого количества изображений.
+3:Подключение скрипта и стиля библиотеки модного окна basicLightbox . 
+ Используйте сервис CDN jsdelivr и добавьте в проект ссылки на минифицированные ( .min) файлы.
+4:Открытие модального окна по клику на элементе украшения. Для этого ознакомьтесь с документацией и примерами .
+5:Замена значения атрибута srcэлемента <img>в модальном окне перед открытием. 
+ Использовал готовую разметку модального окна с изображением из библиотеки библиотеки basicLightbox . */
